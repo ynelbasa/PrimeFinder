a5308e2ef2e47168a0f0725d5f5bf71c080c1952
Ynel Basa <marinelbasa@gmail.com>
2022-04-30 06:16:05 +1200

    Implement prime number validator

---
+++ PrimeFinder.Tests/ValidatorTests.cs
@@ -0,0 +1,48 @@
+using FluentAssertions;
+using Xunit;
+
+namespace PrimeFinder.Tests
+{
+    public class ValidatorTests
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-1001)]
+        public void GivenZeroOrNegativeProduct_WhenIsProductValidCalled_ThenShouldReturnFalse(long product)
+        {
+            var isValid = Validator.IsProductValid(product);
+            isValid.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData(001)]
+        [InlineData(1122)]
+        [InlineData(11223344)]
+        public void GivenProductWithLessThan12Digits_WhenIsProductValidCalled_ThenShouldReturnFalse(long product)
+        {
+            var isValid = Validator.IsProductValid(product);
+            isValid.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData(0012345678910)]
+        [InlineData(1122334455667788)]
+        public void GivenProductWithMoreThan12Digits_WhenIsProductValidCalled_ThenShouldReturnFalse(long product)
+        {
+            var isValid = Validator.IsProductValid(product);
+            isValid.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData(113445566777)]
+        [InlineData(998876554321)]
+        [InlineData(112344556567)]
+        [InlineData(111224445678)]
+        public void GivenDigitsNotSequentiallyAscendingOrEqual_WhenIsProductValidCalled_ThenShouldReturnFalse(long product)
+        {
+            var isValid = Validator.IsProductValid(product);
+            isValid.Should().BeFalse();
+        }
+    }
+}
---
+++ PrimeFinder/Validator.cs
@@ -0,0 +1,37 @@
+﻿namespace PrimeFinder
+{
+    public static class Validator
+    {
+        public static bool IsProductValid(long product)
+        {
+            var isProductTwelveDigits = product > 100000000000 && product < 999999999999;
+
+            return isProductTwelveDigits && IsProductDigitSequencingValid(product);
+        }
+
+        private static bool IsProductDigitSequencingValid(long product)
+        {
+            int size = 12;
+            long[] digits = new long[size];
+            for (int index = size - 1; index >= 0; index--)
+            {
+                digits[index] = (product % 10);
+                product /= 10;
+
+                var isLastDigit = index == size - 1;
+                if (!isLastDigit)
+                {
+                    var previousNumber = digits[index + 1];
+                    var currentNumber = digits[index];
+
+                    var isSameAsPrevious = currentNumber == previousNumber;
+                    var isSequential = currentNumber == previousNumber - 1;
+                    if (!isSameAsPrevious && !isSequential)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
